38cf13be2e67c960a091d01cb80179ee5af4929c
AlexBlejan <alexandrublejan@yahoo.com>
2024-03-13 19:11:37 +0200

    Add example for a function that calculates th sum and another function  that checks if an array has a specific element

--- script.js
+++ script.js
@@ -9,3 +9,41 @@ function printDigits() {
 printDigits();
 printDigits();
 printDigits();
+
+// functii cu parametrii
+
+function printSum(a, b) {
+	let sum = a + b;
+	console.log(`Suma dintre ${a} si ${b} este ${sum}`);
+}
+
+printSum(3, 5);
+
+// un numar se afla intr un Array
+
+function arrayHasElement(arrayToCkeck, Element) {
+	let found = false;
+	for (const el of arrayToCkeck) {
+		if (el === Element) {
+			console.log(`Array ${arrayToCkeck} has element ${Element}`);
+			found = true;
+			break;
+		}
+		if (found) {
+			console.log(`Array ${arrayToCkeck} has element ${Element}`);
+		} else {
+			console.log(`Array ${arrayToCkeck} does not have element ${Element}`);
+		}
+	}
+}
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [3, 10, 12, 21];
+const valueToCheck1 = 10;
+const valueToCheck2 = 30;
+
+arrayHasElement(array1, valueToCheck1);
+arrayHasElement(array2, valueToCheck2);
+
+// alta metoda
+arrayHasElement([`ala`, `bala`, `portocala`], `ala`);
